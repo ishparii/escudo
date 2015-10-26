74ac3edf7a6ca3f3568ca9c0493ed493a23d077e
Jorge Yanar <jorgeyanar@gmail.com>
2015-10-25 22:11:07 -0500

    added pcap parsing demonstration using dpkt
    Note that using the same code on 'test.pcap' fails, likely because the
    packets in test.pcap are using some other kind of protocol.
    (specifically, running it causes eth.data to return a string type,
    causing ip.data to raise an error since .data is not a method for
    strings).

---
+++ using_dpkt.py
@@ -0,0 +1,25 @@
+# A demonstration of how to use dpkt to inspect packets. This particular
+# pcap file contains the TCP/HTTP of a file transfer. For a more detailed
+# explanation of the process, see Jon Oberheide's tutorial at:
+# https://jon.oberheide.org/blog/2008/10/15/dpkt-tutorial-2-parsing-a-pcap-file/
+
+import dpkt
+
+f = open('tcp-http.pcap')
+pcap = dpkt.pcap.Reader(f)
+
+for ts, buf in pcap:        # For timestamp, packet length in the pcap file
+    eth = dpkt.ethernet.Ethernet(buf) # Extract into ethernet object
+    ip = eth.data                     # packet.data is the IP object
+    tcp = ip.data                     # packet.data.data is the TCP object
+
+    # If the destination port is 80 (indicating an HTTP request) and there
+    # exists data beyond the TCP layer available for parsing:
+    if tcp.dport == 80 and len(tcp.data) > 0:
+        http = dpkt.http.Request(tcp.data)
+        print(http.uri)
+        print(http.version)
+        print(http.method)
+        print(http.headers['user-agent'])
+
+f.close()
